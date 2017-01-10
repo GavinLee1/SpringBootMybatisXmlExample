3f407e0c19a0eea84093953ab886b7bb70774b08
ligaozhao <ligaozhao@mozat.com>
2017-01-10 15:03:09 +0800

    Add a test case.

---
+++ src/test/java/com/study/webApi/repository/mybatis/AccountLevelTest.java
@@ -0,0 +1,37 @@
+package com.study.webApi.repository.mybatis;
+
+import javax.inject.Inject;
+
+import com.study.webApi.repository.mybatis.impl.AccountLevelRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.study.infrastructure.exception.DatabaseException;
+import com.study.webApi.repository.mybatis.interf.IAccountLevelRepository;
+import com.study.webApi.repository.pojo.AccountLevel;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+public class AccountLevelTest {
+//	@Inject
+	IAccountLevelRepository repository;
+	
+	@Before
+	public void setUp() {
+		repository = new AccountLevelRepository();
+	}
+	
+	@Test
+	public void testFindAcountLevel() {
+		try {
+			AccountLevel test = repository.find(111);
+			assertEquals(test.getId().intValue(),111);
+			assertEquals(test.getPoint(),10);
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+		}
+	}
+}
